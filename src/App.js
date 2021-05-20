a29a842915fc3530e6b82f2a75a9f317a6f4651c
Shazeen15 <shazzy.sterlin@gmail.com>
2021-05-20 11:19:03 -0700

    displaying x and os on the clicked square

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import React, { Component } from 'react'
-import Square from './components/Square'
-import './App.css'
+import React, { Component } from "react";
+import Square from "./components/Square";
+import "./App.css";
 
-class App extends Component{
-  constructor(props){
-    super(props)
+class App extends Component {
+  constructor() {
+    super();
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    }
+      squares: ["", "", "", "", "", "", "", "", ""],
+      isX: true,
+    };
   }
 
-  render(){
-    return(
+  clickxo = (index) => {
+    const { squares, isX } = this.state;
+    this.setState({
+      isX: !this.state.isX,
+    });
+    squares[index] = isX ? "X" : "O";
+  };
+
+  render() {
+    return (
       <>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div className="gameBaord">
+          {/* 
+        to get the 3X3 squares,
+        I mapped over the squares array
+         */}
+          {this.state.squares.map((val, index) => {
+            return (
+              <Square
+                value={val}
+                index={index}
+                clickxo={this.clickxo}
+                key={index}
+              />
+            );
+          })}
+        </div>
+        <p>Turn: {this.state.isX ? "X" : "O"}</p>
       </>
-    )
+    );
   }
 }
-export default App
+export default App;
